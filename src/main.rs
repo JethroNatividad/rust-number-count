0bf6a3954aadee8336e1aa22c285ad9a84a09352
JethroNatividad <natividad.jet@gmail.com>
2023-12-08 11:08:32 +0800

    add gui

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-// use std::io;
 use eframe::egui;
 
 // Create a program that prompts for an input string and dis- plays output that shows the input string and the number of characters the string contains.
@@ -17,14 +16,6 @@ use eframe::egui;
 // Expected Output: Homer has 5 characters
 
 
-// fn main() -> io::Result<()> {
-//     let mut input = String::new();
-//     io::stdin().read_line(&mut input)?;
-//     println!("{} has {} characters", input.trim(), input.trim().chars().count());
-//     Ok(())
-// }
-
-
 fn main() -> Result<(), eframe::Error> {
     // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
@@ -34,22 +25,19 @@ fn main() -> Result<(), eframe::Error> {
     };
 
     // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
+    let mut text = "".to_owned();
 
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+    eframe::run_simple_native("Text Counter", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                age += 1;
-            }
-            ui.label(format!("Hello '{name}', age {age}"));
+            ui.heading("Count the number of characters");
+
+            let text_label = ui.label("Enter Text: ");
+            ui.text_edit_singleline(&mut text)
+                .labelled_by(text_label.id);
+            
+            if text.len() > 0 {
+                ui.label(format!("{} has {} characters", text, text.chars().count()));
+            };
         });
     })
 }
